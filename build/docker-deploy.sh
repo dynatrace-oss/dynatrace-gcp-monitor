6cc4f6e1268022ceae1a69c513b2f6200b6b00c6
pawel.siwek <pawel.siwek@dynatrace.com>
2020-10-12 09:48:47 +0200

    tagging only if $TAG is passed

--- build/docker-deploy.sh
+++ build/docker-deploy.sh
@@ -2,8 +2,8 @@
 set -eu
 
 docker build -f dockerfile -t dynatrace-gcp-function .        
-docker tag dynatrace-gcp-function dynatrace/dynatrace-gcp-function:$TAG
 
 if [[ "${PUSH:-}" == "true" ]]; then
+    docker tag dynatrace-gcp-function dynatrace/dynatrace-gcp-function:$TAG
     docker push dynatrace/dynatrace-gcp-function:$TAG
 fi
