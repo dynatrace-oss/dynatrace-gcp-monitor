5326d238123a3043b1c5741932b20f3d5ecc69a4
pawel.siwek <pawel.siwek@dynatrace.com>
2020-10-12 09:17:57 +0200

    moved docker deployment into sh script

---
+++ build/docker-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+docker build -f dockerfile -t dynatrace-gcp-function .        
+docker tag dynatrace-gcp-function dynatrace/dynatrace-gcp-function:$TAG
+
+if [[ "${PUSH:-}" == "true" ]]; then
+    docker push dynatrace/dynatrace-gcp-function:$TAG
+fi
