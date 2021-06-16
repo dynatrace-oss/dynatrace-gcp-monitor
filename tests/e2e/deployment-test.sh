9c9bf18d3993a77d9e40f0b1a4da18b530c464ea
Marek Garwolinski <marek.garwolinski@dynatrace.com>
2021-06-16 18:23:28 +0200

    [APM-303450] E2E deployment test

--- tests/e2e/deployment-test.sh
+++ tests/e2e/deployment-test.sh
@@ -13,6 +13,29 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+# Install kubectl.
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && sudo mv kubectl /usr/local/bin/kubectl
+
+# Install helm.
+wget https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz
+FILE=./helm-v3.5.3-linux-amd64.tar.gz
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist. Can't create helm chart."
+    exit 1
+fi
+tar -zxvf helm-v3.5.3-linux-amd64.tar.gz
+FILE=./linux-amd64/helm
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist. Can't create helm chart."
+    exit 1
+fi
+
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
+
+# Create Pub/Sub topic and subscription.
+gcloud config set project dynatrace-gcp-extension
+
 if [[ $(gcloud pubsub topics list --filter=name:"${PUBSUB_TOPIC}" --format="value(name)") ]]; then
     echo "Topic [${PUBSUB_TOPIC}] already exists, skipping"
 else
@@ -25,6 +48,8 @@ else
     gcloud pubsub subscriptions create "${PUBSUB_SUBSCRIPTION}" --topic="${PUBSUB_TOPIC}" --ack-deadline=120
 fi
 
+
+# Run helm deployment.
 rm -rf ./e2e_test
 mkdir ./e2e_test
 cp ./scripts/deploy-helm.sh ./e2e_test/deploy-helm.sh
@@ -39,6 +64,7 @@ sed -i '/^dynatraceLogIngestUrl:/c\dynatraceLogIngestUrl: "'${DYNATRACE_LOG_INGE
 sed -i '/^logsSubscriptionId:/c\logsSubscriptionId: "'${PUBSUB_SUBSCRIPTION}'"' ${VALUES_FILE}
 sed -i '/^requireValidCertificate:/c\requireValidCertificate: "false"' ${VALUES_FILE}
 
+gcloud container clusters get-credentials "${K8S_CLUSTER}" --region us-central1 --project ${GCP_PROJECT_ID}
 
 chmod +x ./e2e_test/deploy-helm.sh
 ./e2e_test/deploy-helm.sh --service-account e2e-test-dynatrace-gcp-function-sa --role-name e2e_test_dynatrace_function
