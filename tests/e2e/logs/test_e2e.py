672d5908203e8541f4ae5b363966690a693f435f
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-07-14 14:52:24 +0200

    add specific query plus test env vars

--- tests/e2e/logs/test_e2e.py
+++ tests/e2e/logs/test_e2e.py
@@ -9,30 +9,43 @@
 #     Unless required by applicable law or agreed to in writing, software
 #     distributed under the License is distributed on an "AS IS" BASIS,
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
+#     See the License for the specassertic language governing permissions and
 #     limitations under the License.
 
 import os
 import requests
+import pytest
 
-DYNATRACE_URL = os.environ['DYNATRACE_URL']
-DYNATRACE_ACCESS_KEY = os.environ['DYNATRACE_ACCESS_KEY']
-START_TIME = os.environ['START_LOAD_GENERATION']
-END_TIME = os.environ['END_LOAD_GENERATION']
+def test_environment_vars():
+    assert "DYNATRACE_URL" in os.environ
+    assert "DYNATRACE_ACCESS_KEY" in os.environ
+    assert "DEPLOYMENT_TYPE" in os.environ
+    assert "TRAVIS_BUILD_ID" in os.environ
+    assert "START_TIME" in os.environ
+    assert "END_TIME" in os.environ
 
+@pytest.fixture
+def get_environments_values():
+    return { 
+        'DYNATRACE_URL': os.environ.get('DYNATRACE_URL'),
+        'DYNATRACE_ACCESS_KEY': os.environ.get('DYNATRACE_ACCESS_KEY'),
+        'DEPLOYMENT_TYPE': os.environ.get('DEPLOYMENT_TYPE'),
+        'TRAVIS_BUILD_ID': os.environ.get('TRAVIS_BUILD_ID'),
+        'START_TIME': os.environ.get('START_TIME'),
+        'END_TIME': os.environ.get('END_TIME'),
+    }
 
-def test_logs_on_dynatrace():
-    url = f"{DYNATRACE_URL}api/v2/logs/search"
+def test_logs_on_dynatrace(get_environments_values):
+    url = f"{get_environments_values['DYNATRACE_URL']}api/v2/logs/search"
     params = {
-        'from': START_TIME,
-        'to': END_TIME,
-        'query': 'content="This is sample app"'
+        'from': get_environments_values['START_TIME'],
+        'to': get_environments_values['END_TIME'],
+        'query': f"content='TYPE: {get_environments_values['DEPLOYMENT_TYPE']}, BUILD: {get_environments_values['TRAVIS_BUILD_ID']}, INFO: This is sample app'"
     }
     headers = {
-        'Authorization': f"Api-Token {DYNATRACE_ACCESS_KEY}"
+        'Authorization': f"Api-Token {get_environments_values['DYNATRACE_ACCESS_KEY']}"
     }
     resp = requests.get(url, params=params, headers=headers)
 
     assert resp.status_code == 200
     assert len(resp.json()['results']) == 5
-    assert resp.json()['results'][0]['content'] == "This is sample app"
