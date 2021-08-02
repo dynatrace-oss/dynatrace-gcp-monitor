5b376d89b442e04aa2e366f09468ad52285dc56f
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-08-02 08:28:26 +0200

    remove run version script

--- build/docker-deploy.sh
+++ build/docker-deploy.sh
@@ -14,7 +14,6 @@ if [[ "${PUSH:-}" == "true" ]]; then
     docker tag dynatrace/dynatrace-gcp-function:latest dynatrace/dynatrace-gcp-function:$TAG
     docker push dynatrace/dynatrace-gcp-function --all-tags
 elif [[ "${PUSH:-}" != "true" && "${E2E:-}" == "true" ]]; then
-    ./build/version.sh
     docker tag dynatrace/dynatrace-gcp-function ${GCR_NAME}:e2e-travis-test-${TRAVIS_BUILD_ID}
     docker push ${GCR_NAME}:e2e-travis-test-${TRAVIS_BUILD_ID}
 fi
