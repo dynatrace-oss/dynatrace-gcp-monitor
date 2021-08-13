24ef1c2ae67671394578269f88818bf8a24d64a5
Maria Swiatkowska <72859155+mswiatkowska@users.noreply.github.com>
2021-08-13 10:03:39 +0200

    APM-318761: Set default severity to INFO (#155)

--- tests/unit/extraction_rules/test_default.py
+++ tests/unit/extraction_rules/test_default.py
@@ -18,7 +18,7 @@ from typing import NewType, Any
 from lib.logs import logs_processor
 from lib.logs.metadata_engine import ATTRIBUTE_GCP_PROJECT_ID, ATTRIBUTE_GCP_RESOURCE_TYPE, ATTRIBUTE_CLOUD_PROVIDER, \
     ATTRIBUTE_CLOUD_REGION, ATTRIBUTE_GCP_REGION, ATTRIBUTE_CONTENT, ATTRIBUTE_TIMESTAMP, \
-    ATTRIBUTE_DT_LOGPATH
+    ATTRIBUTE_DT_LOGPATH, ATTRIBUTE_SEVERITY
 from unit.extraction_rules.common import TEST_LOGS_PROCESSING_CONTEXT
 
 MonkeyPatchFixture = NewType("MonkeyPatchFixture", Any)
@@ -56,7 +56,8 @@ expected_output = {
     ATTRIBUTE_GCP_RESOURCE_TYPE: 'cloud_run_revision',
     ATTRIBUTE_TIMESTAMP: timestamp,
     ATTRIBUTE_CONTENT: record_string,
-    ATTRIBUTE_DT_LOGPATH: 'projects/dynatrace-gcp-extension/logs/run.googleapis.com%2Fstdout'
+    ATTRIBUTE_DT_LOGPATH: 'projects/dynatrace-gcp-extension/logs/run.googleapis.com%2Fstdout',
+    ATTRIBUTE_SEVERITY: "INFO"
 }
 
 expected_output_attribute_values_trimmed = {
@@ -67,7 +68,8 @@ expected_output_attribute_values_trimmed = {
     ATTRIBUTE_GCP_RESOURCE_TYPE: 'clou',
     ATTRIBUTE_TIMESTAMP: timestamp,
     ATTRIBUTE_CONTENT: record_string,
-    ATTRIBUTE_DT_LOGPATH: 'proj'
+    ATTRIBUTE_DT_LOGPATH: 'proj',
+    ATTRIBUTE_SEVERITY: "INFO"
 }
 
 
