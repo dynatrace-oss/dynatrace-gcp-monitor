c8dc9f8fecdfebda6b0b873299f94c11a86cf4c9
Wladyslaw Wolinski <wladyslaw.wolinski@dynatrace.com>
2021-08-31 21:10:59 +0200

    [APM-297894] Improve diagnostics of GCP metric ingest function - fixed false positive GitGuard token exposure warning.

--- tests/unit/test_fast_check.py
+++ tests/unit/test_fast_check.py
@@ -4,12 +4,12 @@ from lib.fast_check import format_dynatrace_access_key
 
 
 def test_token_masking_old_format():
-    assert_that(format_dynatrace_access_key("UB7CLJy7RQS-qZD1wfCfX")).is_equal_to("UB7***************CfX")
+    assert_that(format_dynatrace_access_key("FAKEFAKE7FA-keFAkefAke")).is_equal_to("FAK****************Ake")
 
 
 def test_token_masking_new_format():
     assert_that(format_dynatrace_access_key(
-        "dt0c01.FAKEEU7QQ5KW7BZVN2XQ7KD7.FAKEJFHLZKTOUTFZKAAZTRSM7HFKPHFRCJKJN5CWTHMFD7UNE77TPZ7PE77WA7JY")).is_equal_to(
+        "dt0c01.FAKEEU7QQ5KW7BZVN2XQFAKE.FAKEJFHLZKTOUTFZKAAZTRSM7HFKPHFRCJKJN5CWTHMFAKEFAKEFAKEPEFAKE7JY")).is_equal_to(
         "dt0c01.FAK***********************************************************************************7JY")
 
 
