61b5dd2ab385ce32b4939db3080de8e29cba77ad
Marek Garwolinski <marek.garwolinski@dynatrace.com>
2021-09-01 11:04:16 +0200

    Update src/lib/context.py
    Co-authored-by: Paweł Gutkowski <pawel.gutkowski@dynatrace.com>

--- src/lib/context.py
+++ src/lib/context.py
@@ -49,7 +49,7 @@ def get_query_interval_minutes() -> int:
     else:
         # keep old query frequency for logs ingest or if cloud function code was updated without changing cloud scheduler (missing environment variable)
         query_interval_min = 1
-    if not 1 <= query_interval_min <= 6:
+    if query_interval_min not in range(1, 7):
         query_interval_min = default_query_interval
     return query_interval_min
 
