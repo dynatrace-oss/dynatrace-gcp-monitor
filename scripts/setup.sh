da3d67eb7ca74ac157b1f44a8f86c5c781f0f76b
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-10-07 13:18:37 +0200

    fix push and activation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -227,16 +227,17 @@ upload_extension_to_cluster() {
   EXTENSION_ZIP=$3
   EXTENSION_VERSION=$4
 
-  UPLOAD_RESPONSE=$(curl -s -k -X POST "$DYNATRACE_URL/api/v2/extensions" -w "<<HTTP_CODE>>%{http_code}" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $DYNATRACE_ACCESS_KEY" -H "Content-Type: multipart/form-data" -F "file=@extensions/$EXTENSION_ZIP;type=application/zip")
+  UPLOAD_RESPONSE=$(curl -s -k -X POST "$DYNATRACE_URL/api/v2/extensions" -w "<<HTTP_CODE>>%{http_code}" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $DYNATRACE_ACCESS_KEY" -H "Content-Type: multipart/form-data" -F "file=@$EXTENSION_ZIP;type=application/zip")
   CODE=$(sed -rn 's/.*<<HTTP_CODE>>(.*)$/\1/p' <<<"$UPLOAD_RESPONSE")
-  if [ "$CODE" -gt "310" ]; then
+
+  if [[ "$CODE" -gt "310" ]]; then
     warn "- Extension $EXTENSION_ZIP upload failed with error code: $CODE"
   else
     UPLOADED_EXTENSION=$(echo "$UPLOAD_RESPONSE" | sed -r 's/<<HTTP_CODE>>.*$//' | jq -r '.extensionName')
     ACTIVATION_RESPONSE=$(curl -s -k -X PUT "$DYNATRACE_URL/api/v2/extensions/${UPLOADED_EXTENSION}/environmentConfiguration" -w "<<HTTP_CODE>>%{http_code}" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $DYNATRACE_ACCESS_KEY" -H "Content-Type: application/json" --data-raw "{\"version\": \"${EXTENSION_VERSION}\"}")
     CODE=$(sed -rn 's/.*<<HTTP_CODE>>(.*)$/\1/p' <<<"$ACTIVATION_RESPONSE")
 
-    if [ "$CODE" -gt "310" ]; then
+    if [[ "$CODE" -gt "310" ]]; then
       warn "- Activation $UPLOADED_EXTENSION:$EXTENSION_VERSION failed with error code: $CODE"
     else
       echo "- Extension $UPLOADED_EXTENSION:$EXTENSION_VERSION activated."
@@ -251,7 +252,7 @@ get_activated_extensions_on_cluster() {
   if RESPONSE=$(curl -k -s "$DYNATRACE_URL/api/v2/extensions" -w "<<HTTP_CODE>>%{http_code}" -H "Accept: application/json; charset=utf-8" -H "Content-Type: application/json; charset=utf-8" -H "Authorization: Api-Token $DYNATRACE_ACCESS_KEY" --connect-timeout 20); then
     CODE=$(sed -rn 's/.*<<HTTP_CODE>>(.*)$/\1/p' <<<"$RESPONSE")
 
-    if [ "$CODE" -gt "310" ]; then
+    if [[ "$CODE" -gt "310" ]]; then
       err "- Dynatrace Cluster at: $DYNATRACE_URL/api/v2/extensions response with code: $CODE"
       exit
     fi
