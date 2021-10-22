45a5485587e4704b042e986e6a63f4aad05875b5
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-10-22 10:19:14 +0200

    clean test

--- tests/e2e/deployment-test-cleanup.sh
+++ tests/e2e/deployment-test-cleanup.sh
@@ -25,7 +25,6 @@ gcloud container images delete "${GCR_NAME}:e2e-travis-test-${TRAVIS_BUILD_ID}"
 gcloud functions delete "${CLOUD_FUNCTION_NAME}"
 
 # testing message
-echo curl -s -k -X GET "${DYNATRACE_URL}api/v2/extensions" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token xxxxxxxxxxxxx"
 INSTALLED_EXTENSIONS=$(curl -s -k -X GET "${DYNATRACE_URL}api/v2/extensions" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token ${DYNATRACE_ACCESS_KEY}" | jq -r '.extensions[].extensionName')
 
 for extension in ${INSTALLED_EXTENSIONS}; do
@@ -34,6 +33,6 @@ for extension in ${INSTALLED_EXTENSIONS}; do
     echo "Deactivated configuration version:"
     curl -s -k -X DELETE "${DYNATRACE_URL}api/v2/extensions/${extension}/environmentConfiguration" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token ${DYNATRACE_ACCESS_KEY}" | jq -r '.version'
     echo "Extension deleted:"
-    curl -s -k -X DELETE "${DYNATRACE_URL}api/v2/extensions/${extension}/${VERSION}" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token ${DYNATRACE_ACCESS_KEY}" | jq -r '.extensions[] | "\(.extensionName):\(.version)"'
+    curl -s -k -X DELETE "${DYNATRACE_URL}api/v2/extensions/${extension}/${VERSION}" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token ${DYNATRACE_ACCESS_KEY}"
     echo
 done
