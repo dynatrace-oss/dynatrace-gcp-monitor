2c565f818624201bbd83afe9c981c2539a24ba4a
Maria Swiatkowska <72859155+mswiatkowska@users.noreply.github.com>
2021-11-10 12:59:17 +0100

    Added parameter resolution 'Inf' (#221)

--- tests/e2e/metrics/test_metrics.py
+++ tests/e2e/metrics/test_metrics.py
@@ -33,7 +33,8 @@ def test_metrics_on_dynatrace():
     url = f"{os.environ['DYNATRACE_URL'].rstrip('/')}/api/v2/metrics/query"
     params = {'from': os.environ['START_LOAD_GENERATION'],
               'to': os.environ['END_LOAD_GENERATION'],
-              'metricSelector': f"cloud.gcp.cloudfunctions_googleapis_com.function.execution_count:filter(eq(gcp.instance.name, {os.environ['CLOUD_FUNCTION_NAME']}),eq(gcp.project.id, {os.environ['GCP_PROJECT_ID']}))"
+              'metricSelector': f"cloud.gcp.cloudfunctions_googleapis_com.function.execution_count:filter(eq(gcp.instance.name, {os.environ['CLOUD_FUNCTION_NAME']}),eq(gcp.project.id, {os.environ['GCP_PROJECT_ID']}))",
+              'resolution': 'Inf'
               }
     headers = {
         'Authorization': f"Api-Token {os.environ['DYNATRACE_ACCESS_KEY']}"
