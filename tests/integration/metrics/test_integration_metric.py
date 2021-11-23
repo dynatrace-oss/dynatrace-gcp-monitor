05172ab796b5c1a9e48643c96c87b68e215dfada
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-11-23 14:00:01 +0100

    fix tests

--- tests/integration/metrics/test_integration_metric.py
+++ tests/integration/metrics/test_integration_metric.py
@@ -136,4 +136,5 @@ async def test_ingest_lines_output(resource_path_root):
         recorded_ingest = ingest.read()
 
         assert_that(body.split("\n")).is_length(289)
-        assert_that(body).is_equal_to(recorded_ingest)
+        for line in body.split("\n"):
+            assert line in recorded_ingest
