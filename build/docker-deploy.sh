be861ff9fafac276ddc18443c66316ba32b169fe
Karol Kieglerski <karol.kieglerski@dynatrace.com>
2021-12-01 13:52:36 +0100

    Push tag separate

--- build/docker-deploy.sh
+++ build/docker-deploy.sh
@@ -11,8 +11,9 @@ if [[ "${PUSH:-}" == "true" ]]; then
     touch ~/.docker/config.json && chmod 0600 ~/.docker/config.json
     base64 -d >~/.docker/config.json <<<"$OAO_DOCKER_AUTH"
 
-    docker tag dynatrace/dynatrace-gcp-function:latest dynatrace/dynatrace-gcp-function:$TAG
-    docker push dynatrace/dynatrace-gcp-function
+    docker tag dynatrace/dynatrace-gcp-function:latest dynatrace/dynatrace-gcp-function:${TAG}
+    docker push dynatrace/dynatrace-gcp-function:latest
+    docker push dynatrace/dynatrace-gcp-function:${TAG}
 elif [[ "${PUSH:-}" != "true" && "${E2E:-}" == "true" ]]; then
     docker tag dynatrace/dynatrace-gcp-function ${GCR_NAME}:e2e-travis-test-${TRAVIS_BUILD_ID}
     docker push ${GCR_NAME}:e2e-travis-test-${TRAVIS_BUILD_ID}
