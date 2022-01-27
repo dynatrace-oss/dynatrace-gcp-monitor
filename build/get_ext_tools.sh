9d356a9b61532e59d5d8c20a4bde37babcc565b2
Marcin Wolny <marcin.wolny@dynatrace.com>
2022-01-27 09:57:01 +0100

    PR remarks
    Download or cache dependencies

--- build/get_ext_tools.sh
+++ build/get_ext_tools.sh
@@ -1,5 +1,38 @@
-EXT_DIR=ext_tools
+#!/bin/bash
+#     Copyright 2021 Dynatrace LLC
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+set -e
 
+EXT_DIR=ext_tools
+WGET_CACHE=.wgetcache
+
+wget_cache(){
+  URL=$1
+  TARGET=$2
+
+  FILE="${WGET_CACHE}/${URL/https:\/\//}"
+  if [ ! -f "$FILE" ]; then
+    echo "Downloading $URL ..."
+    mkdir -p "$(dirname "$FILE" )"
+    wget -nv "${URL}" -O "$FILE"
+
+  else
+    echo "Using cached $URL -> $FILE"
+  fi
+
+  cp "$FILE" "$TARGET"
+}
 
 get_ext() {
 URL=$1
@@ -13,11 +46,13 @@ do
    ARCH_DST=$2
 
    FILE="$EXT_DIR/${NAME}_${ARCH_DST}"
-   wget -v "${URL}${ARCH_SRC}" -O "$FILE" && chmod +x "$FILE"
+   wget_cache "${URL}${ARCH_SRC}" "$FILE" && chmod +x "$FILE"
    shift 2
 done
 }
 
-get_ext https://github.com/stedolan/jq/releases/download/jq-1.6/jq jq -linux64 linux_x64
-
+get_ext https://github.com/stedolan/jq/releases/download/jq-1.5/jq jq -linux64 linux_x64
 get_ext https://github.com/mikefarah/yq/releases/download/v4.9.8/yq yq _linux_amd64 linux_x64
+
+cd ext_tools
+sha256sum -c sha256sums.txt
