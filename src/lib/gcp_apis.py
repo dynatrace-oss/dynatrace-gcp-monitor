9e91e6667bf790fcb1d992ed227777d2c8c66e4d
equardo <szym.trzebiatowski@gmail.com>
2022-03-25 16:21:51 +0100

    remove test logs

--- src/lib/gcp_apis.py
+++ src/lib/gcp_apis.py
@@ -24,14 +24,12 @@ async def get_all_disabled_apis(context: MetricsContext, token: str, project_id:
     disabled_apis = set()
     try:
         response = await context.gcp_session.get(base_url, headers=headers, raise_for_status=True)
-        context.log(f'Initial response {response}')
         disabled_services_json = await response.json()
         disabled_services = disabled_services_json.get("services", [])
         disabled_apis.update({disable_service.get("config", {}).get("name", "") for disable_service in disabled_services})
         while disabled_services_json.get("nextPageToken"):
             url = f"{base_url}&pageToken={disabled_services_json['nextPageToken']}"
             response = await context.gcp_session.get(url, headers=headers, raise_for_status=True)
-            context.log(f'Continued response {response}')
             disabled_services_json = await response.json()
             disabled_services = disabled_services_json.get("services", [])
             disabled_apis.update({disable_service.get("config", {}).get("name", "") for disable_service in disabled_services})
