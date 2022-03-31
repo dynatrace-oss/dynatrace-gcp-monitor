c2e00a84717395095657aeab906987f42d70188e
Joaquin Filipic <joaquin.filipic@dynatrace.com>
2022-03-31 11:49:59 +0200

    Increase waiting time to 10 min

--- tests/e2e/metrics/test_metrics.py
+++ tests/e2e/metrics/test_metrics.py
@@ -27,8 +27,8 @@ def test_environment_vars():
 
 
 def test_metrics_on_dynatrace():
-    print(f"Try to receive execution_count metric from Dynatrace in 5 min (start_time={os.environ['START_LOAD_GENERATION']} ,end_time={os.environ['END_LOAD_GENERATION']})")
-    time.sleep(7*60)
+    print(f"Try to receive execution_count metric from Dynatrace in 10 min (start_time={os.environ['START_LOAD_GENERATION']} ,end_time={os.environ['END_LOAD_GENERATION']})")
+    time.sleep(10*60)
 
     url = f"{os.environ['DYNATRACE_URL'].rstrip('/')}/api/v2/metrics/query"
     params = {'from': os.environ['START_LOAD_GENERATION'],
