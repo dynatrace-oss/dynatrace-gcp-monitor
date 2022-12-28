9a9614c830a08f230f6c9310d0ce0ae438702c04
Joaquin Filipic <joaquin.filipic@dynatrace.com>
2022-12-28 11:20:20 +0100

    Refactor

--- src/lib/webserver/webserver.py
+++ src/lib/webserver/webserver.py
@@ -2,25 +2,34 @@ from aiohttp import web
 from aiohttp.web_app import Application
 from aiohttp.web_runner import AppRunner
 
-from lib.context import LoggingContext
+from lib.context import LoggingContext, get_int_environment_value
 
 logging_context = LoggingContext("webserver")
 
 APP: Application = None
 RUNNER: AppRunner = None
 
+HEALTH_CHECK_PORT = get_int_environment_value("HEALTH_CHECK_PORT", 8080)
 
-def setup_webserver_on_asyncio_loop(loop, port):
-    global APP, RUNNER
-    logging_context.log("Setting up webserver... \n")
 
-    APP = web.Application()
-    APP.add_routes([web.get('/health', health_endpoint)])
+def run_webserver_on_asyncio_loop_forever(loop):
+    try:
+        global APP, RUNNER
+        logging_context.log("Setting up webserver... \n")
 
-    RUNNER = web.AppRunner(APP)
-    loop.run_until_complete(RUNNER.setup())
-    site = web.TCPSite(RUNNER, '0.0.0.0', port)
-    loop.run_until_complete(site.start())
+        APP = web.Application()
+        APP.add_routes([web.get('/health', health_endpoint)])
+
+        RUNNER = web.AppRunner(APP)
+        loop.run_until_complete(RUNNER.setup())
+        site = web.TCPSite(RUNNER, '0.0.0.0', HEALTH_CHECK_PORT)
+        loop.run_until_complete(site.start())
+
+        loop.run_forever()
+    finally:
+        print("Closing AsyncIO loop...")
+        close_and_cleanup(loop)
+        loop.close()
 
 
 async def health_endpoint(request):
