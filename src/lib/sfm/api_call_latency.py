96c48b1cb097a6e5d0164e68e0d4c4423ceab4b5
Kozimov <nematullo.kozimov@dynatrace.com>
2023-02-20 07:51:40 +0100

    PCLOUDS-1707 Minor refactors

--- src/lib/sfm/api_call_latency.py
+++ src/lib/sfm/api_call_latency.py
@@ -7,13 +7,14 @@ class ApiCallLatency:
 
     @staticmethod
     def update(api_url, time):
-        if api_url in ApiCallLatency._value:
-            ApiCallLatency._value[api_url].append(time)
-        else:
-            ApiCallLatency._value[api_url] = [time]
+        ApiCallLatency._value[api_url].append(time)
 
     @staticmethod
     def print_statistics(context: LoggingContext):
+        print("Print_statistics has been called :)")
         for api_url, times in ApiCallLatency._value.items():
-            context.log(f"Url: {api_url}, min: {min(times):.3}s, avg: {sum(times) / len(times):.3}s, max: {max(times):.3}s")
-        ApiCallLatency._value = {}
+            context.log(
+                f"Api call latency: [url - {api_url}], [number_of_calls - {len(times)}], "
+                f"[min - {min(times):.3}s, avg - {sum(times) / len(times):.3}s, max - {max(times):.3}s]"
+            )
+        ApiCallLatency._value = defaultdict(list)
