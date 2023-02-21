30089053c14e0f477ad2e414e063abe320a7cc1d
Paskal <nikita.paskal@dynatrace.com>
2023-02-21 10:04:20 +0100

    Helm cleanup

--- tests/e2e/before-deployment-test-helm-cleanup.sh
+++ tests/e2e/before-deployment-test-helm-cleanup.sh
@@ -13,5 +13,6 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+echo "HELM CLEANUP AFTER TEST!!!!!!!!!!!!!!"
 gcloud container clusters get-credentials "${K8S_CLUSTER}" --region us-central1 --project "${GCP_PROJECT_ID}"
 helm -n dynatrace ls --all --short | grep dynatrace-gcp-function | xargs --no-run-if-empty -L1 helm -n dynatrace uninstall --timeout 10m
