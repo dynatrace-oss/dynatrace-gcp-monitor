0597e6d4b2e55814f2d058e8b5b8a4cd5bcac3f1
Paskal <nikita.paskal@dynatrace.com>
2023-03-01 15:30:23 +0100

    Clean logs output

--- gcp-simulator/run.sh
+++ gcp-simulator/run.sh
@@ -4,4 +4,4 @@ export GCP_CLOUD_RESOURCE_MANAGER_URL='http://localhost:8080/cloudresourcemanage
 export GCP_SERVICE_USAGE_URL='http://localhost:8080/serviceusage.googleapis.com/v1'
 export GCP_MONITORING_URL='http://localhost:8080/monitoring.googleapis.com/v3'
 
-INSTANCES=2 PROJECTS=1 SUB_PROJECTS=200 python -m uvicorn main:app  --host 0.0.0.0 --port 8080 & python ../src/dev_local_run.py
+INSTANCES=2 PROJECTS=1 SUB_PROJECTS=200 python -m uvicorn main:app  --host 0.0.0.0 --port 8080 --no-access-log & python ../src/dev_local_run.py
