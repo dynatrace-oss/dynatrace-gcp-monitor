1655e1af56896cd21669ec9583f9efd973505599
nikita.paskal <nikita.paskal@dynatrace.com>
2023-05-08 12:27:37 +0200

    time increase

--- tests/e2e/logs/test_e2e.py
+++ tests/e2e/logs/test_e2e.py
@@ -14,7 +14,7 @@
 
 import os
 import requests
-
+import time
 
 def test_environment_vars():
     assert "DYNATRACE_URL" in os.environ
@@ -52,6 +52,7 @@ def test_logs_on_dynatrace():
     # To query logs via API, a new OAuth token is required, instead of the same
     # DT Token with logs-reading scope.
     # More info: https://www.dynatrace.com/support/help/dynatrace-api/basics/dynatrace-api-authentication/account-api-authentication
+    time.sleep(60)
     url = f"{os.environ.get('DYNATRACE_URL').rstrip('/')}/api/v2/logs/search"
     time_on_cluster_url=f"{os.environ.get('DYNATRACE_URL').rstrip('/')}/api/v1/time"
     time_on_cluster = requests.get(time_on_cluster_url, params={}, headers={})
