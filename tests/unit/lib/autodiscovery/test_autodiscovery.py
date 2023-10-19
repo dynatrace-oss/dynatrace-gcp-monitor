f5b228cb3b6574f1146ef47aa229944d10b42910
Szymon Nagel <szymon.nagel@dynatrace.com>
2023-10-19 11:16:04 +0200

    NOISSUE update

--- tests/unit/lib/autodiscovery/test_autodiscovery.py
+++ tests/unit/lib/autodiscovery/test_autodiscovery.py
@@ -1,7 +1,7 @@
 import asyncio
 from unittest.mock import AsyncMock, Mock, patch
-
 import pytest
+
 from lib.autodiscovery.autodiscovery import AutodiscoveryContext
 from lib.autodiscovery.autodiscovery_task_executor import AutodiscoveryTaskExecutor
 
