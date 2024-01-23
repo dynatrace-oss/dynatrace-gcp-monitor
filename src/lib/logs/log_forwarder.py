2221b296fc5df6846125626cfce280aeb7cb0bf1
Kozimov <nematullo.kozimov@dynatrace.com>
2024-01-23 08:19:40 +0100

    PCLOUDS-3601 Clean up

--- src/lib/logs/log_forwarder.py
+++ src/lib/logs/log_forwarder.py
@@ -14,33 +14,17 @@
 
 import asyncio
 from asyncio import Queue
-import time
-from typing import List, Tuple
 
-from lib.clientsession_provider import init_gcp_client_session, init_dt_client_session
-from lib.context import LoggingContext, LogsProcessingContext, create_logs_context
-from lib.credentials import create_token
+from lib.context import LoggingContext
 from lib.instance_metadata import InstanceMetadata
-from lib.logs.dynatrace_client import DynatraceClient
-from lib.logs.gcp_client import GCPClient
 from lib.logs.log_forwarder_variables import (
     LOGS_SUBSCRIPTION_ID,
     LOGS_SUBSCRIPTION_PROJECT,
     MAX_SFM_MESSAGES_PROCESSED,
     PROCESSING_WORKERS,
-    REQUEST_BODY_MAX_SIZE,
-    NUMBER_OF_CONCURRENT_MESSAGE_PULL_COROUTINES,
-    NUMBER_OF_CONCURRENT_PUSH_COROUTINES,
 )
-
-
-from lib.logs.log_self_monitoring import create_sfm_loop, put_sfm_into_queue
-from lib.logs.logs_processor import (
-    prepare_context_and_process_message,
-    prepare_batches,
-    LogBatch,
-)
-from lib.utilities import chunks
+from lib.logs.log_self_monitoring import create_sfm_loop
+from lib.logs.log_integration_service import LogIntegrationService
 
 
 def run_logs_wrapper(logging_context, instance_metadata, process_number):
@@ -51,10 +35,11 @@ async def run_logs(
     logging_context: LoggingContext, instance_metadata: InstanceMetadata, process_number
 ):
     if not LOGS_SUBSCRIPTION_PROJECT or not LOGS_SUBSCRIPTION_ID:
-        raise Exception(
-            "Cannot start pubsub pulling - GCP_PROJECT or LOGS_SUBSCRIPTION_ID are not defined"
-        )
+        raise Exception("Cannot start pubsub pulling - GCP_PROJECT or LOGS_SUBSCRIPTION_ID are not defined")
+
+    # Each process starts 15 second later than the previous one
     await asyncio.sleep(process_number * 15)
+
     sfm_queue = Queue(MAX_SFM_MESSAGES_PROCESSED)
     log_integration_service = LogIntegrationService(sfm_queue)
     await log_integration_service.update_gcp_client(logging_context)
@@ -66,9 +51,7 @@ async def run_logs(
         )
         tasks.append(worker_task)
 
-    sfm_task = asyncio.create_task(
-        create_sfm_loop(sfm_queue, logging_context, instance_metadata)
-    )
+    sfm_task = asyncio.create_task(create_sfm_loop(sfm_queue, logging_context, instance_metadata))
     tasks.append(sfm_task)
 
     await asyncio.gather(*tasks, return_exceptions=True)
@@ -82,116 +65,16 @@ async def pull_and_push_logs_forever(
     logging_context.log(f"Starting processing")
     while True:
         try:
+            # Pull logs from pub/sub, process them and create batches out of them to send to Dynatrace
             log_batches, ack_ids = await log_integration_service.perform_pull(logging_context)
+
+            # Push logs batches to Dynatrace
             ack_ids_to_send = await log_integration_service.push_logs(log_batches, logging_context)
+
+            # Push ACK_IDs to pub/sub
             ack_ids_to_send.extend(ack_ids)
             await log_integration_service.push_ack_ids(ack_ids_to_send, logging_context)
-            start_time = time.time()
-            del log_batches
-            print(f"Time spent for del: {time.time() - start_time}")
-            del ack_ids_to_send
         except Exception as e:
-            logging_context.exception("Failed to pull messages")
+            logging_context.exception(f"Failed to pull or push messages: {e}")
             # Backoff for 1 minute to avoid spamming requests and logs
             await asyncio.sleep(60)
-
-
-class LogIntegrationService:
-    def __init__(self, sfm_queue):
-        self.gcp_client = None
-        self.dynatrace_client = DynatraceClient()
-        self.sfm_queue = sfm_queue
-        self.log_push_semaphore = asyncio.Semaphore(NUMBER_OF_CONCURRENT_PUSH_COROUTINES)
-
-    async def update_gcp_client(self, logging_context: LoggingContext):
-        async with init_gcp_client_session() as gcp_session:
-            token = await create_token(logging_context, gcp_session)
-            if token is None:
-                raise Exception("Cannot start pubsub pulling - 'Failed to fetch token")
-            self.gcp_client = GCPClient(token)
-
-    async def perform_pull(self, logging_context: LoggingContext) -> Tuple[List[LogBatch], List[str]]:
-        async with init_gcp_client_session() as gcp_session:
-            context = LogsProcessingContext(None, None, self.sfm_queue)
-            context.self_monitoring.pulling_time_start = time.perf_counter()
-
-            tasks_to_pull_messages = [
-                self.gcp_client.pull_messages(logging_context, gcp_session)
-                for _ in range(NUMBER_OF_CONCURRENT_MESSAGE_PULL_COROUTINES)
-            ]
-            responses = await asyncio.gather(
-                *tasks_to_pull_messages, return_exceptions=True
-            )
-
-            context.self_monitoring.calculate_pulling_time()
-            processed_messages = []
-            ack_ids_to_send = []
-
-            context.self_monitoring.processing_time_start = time.perf_counter()
-            for response in responses:
-                if not isinstance(response, Exception):
-                    for received_message in response.get("receivedMessages", []):  # type: ignore
-                        message_job = prepare_context_and_process_message(
-                            self.sfm_queue, received_message
-                        )
-                        if (
-                                not message_job
-                                or message_job.bytes_size > REQUEST_BODY_MAX_SIZE - 2
-                        ):
-                            ack_ids_to_send.append(received_message.get("ackId"))
-                            continue
-                        processed_messages.append(message_job)
-            context.self_monitoring.calculate_processing_time()
-            put_sfm_into_queue(context)
-            return (
-                prepare_batches(processed_messages) if processed_messages else [],
-                ack_ids_to_send,
-            )
-
-    async def push_logs(self, log_batches, logging_context: LoggingContext) -> List[str]:
-        ack_ids_to_send = []
-        context = create_logs_context(self.sfm_queue)
-        batch_length = len(log_batches)
-        if batch_length == 0:
-            logging_context.log(f"Skipping pushing logs - no logs to push")
-            return []
-
-        logging_context.log(
-            f"Number of log batches to push to Dynatrace: {batch_length}"
-        )
-
-        async def process_batch(batch: LogBatch):
-            async with self.log_push_semaphore:
-                await self.dynatrace_client.send_logs(
-                    context, dt_session, batch, ack_ids_to_send
-                )
-
-        context.self_monitoring.sending_time_start = time.perf_counter()
-        async with init_dt_client_session() as dt_session:
-            exceptions = await asyncio.gather(
-                *[process_batch(batch) for batch in log_batches], return_exceptions=True
-            )
-        context.self_monitoring.calculate_sending_time()
-        put_sfm_into_queue(context)
-
-        return ack_ids_to_send
-
-    async def push_ack_ids(self, ack_ids: List[str], logging_context: LoggingContext):
-        # request size limit is 524288, but we are not able to easily control size of created protobuf
-        # empiric test indicates that ack_ids have around 200-220 chars. We can safely assume that ack id is never longer
-        # than 256 chars, we split ack ids into chunks with no more than 2048 ack_id's
-        async with init_gcp_client_session() as gcp_session:
-            chunk_size = 2048
-            tasks_to_send_ack_ids = [
-                self.gcp_client.push_ack_ids(chunk, gcp_session, logging_context)
-                for chunk in chunks(ack_ids, chunk_size)
-            ]
-            exceptions = await asyncio.gather(
-                *tasks_to_send_ack_ids, return_exceptions=True
-            )
-        if ack_ids:
-            logging_context.log("Log ingest payload pushed successfully")
-        if self.sfm_queue.qsize() < 3:
-            await self.update_gcp_client(logging_context)
-
-
