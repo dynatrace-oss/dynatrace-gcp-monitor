77566f65104b6d7111721e33ebea85ff1097b8ec
Joaquin Filipic <joaquin.filipic@dynatrace.com>
2024-08-21 10:13:22 +0200

    TEST: function deploy error

--- tests/e2e/lib-tests.sh
+++ tests/e2e/lib-tests.sh
@@ -22,7 +22,7 @@ create_sample_app() {
   gcloud functions deploy "${CLOUD_FUNCTION_NAME}" \
   --runtime python312 \
   --trigger-http \
-  --no-allow-unauthenticated \
+  --verbosity debug \
   --source ./tests/e2e/sample_app/
 }
 
