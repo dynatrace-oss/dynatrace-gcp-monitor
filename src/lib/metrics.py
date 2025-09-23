dc6e1f82c827fa9040c5c6f0aa962b4238f5f6e8
Szymon Nagel <szymon.nagel@dynatrace.com>
2025-09-23 13:13:29 +0200

    Update default value for gcpMonitoringFilter in GCPService class

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -201,7 +201,7 @@ class GCPService:
             if x.get("gcpOptions", {}).get("valueType", "").upper() != "STRING"
         ])
         object.__setattr__(self, "activation", kwargs.get("activation", {}))
-        monitoring_filter = kwargs.get("gcpMonitoringFilter", "")
+        monitoring_filter = kwargs.get("gcpMonitoringFilter", "var:filter_conditions")
         if self.activation:
             for var_key, var_value in (self.activation.get("vars", {}) or {}).items():
                 monitoring_filter = monitoring_filter.replace(f'{{{var_key}}}', var_value)\
