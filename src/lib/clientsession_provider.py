ce35481721f773759dd06bdb836bb5ea0a84dcc9
Szymon Nagel <szymon.nagel@dynatrace.com>
2026-05-27 11:50:52 +0200

    DAQ-24796: fix client session timeouts using sock_connect/sock_read instead of connect
    - Use connect=None to avoid pool-queuing timeouts (connect includes
      wait time for a free connection slot, causing ConnectionTimeoutError
      under high concurrency like 950 pubsub metric requests)
    - Use sock_connect=30s (aiohttp default since v3.10.9) to detect
      unreachable hosts without penalizing pool queuing
    - Add sock_read=60s to catch stalled mid-response transfers
    - Increase GCP TCPConnector limit from 100 to 500 for better throughput
    - Make all values configurable via environment variables:
      REQUEST_TIMEOUT_TOTAL, REQUEST_TIMEOUT_SOCK_CONNECT,
      REQUEST_TIMEOUT_SOCK_READ, GCP_CONNECTOR_LIMIT

--- src/lib/clientsession_provider.py
+++ src/lib/clientsession_provider.py
@@ -16,6 +16,7 @@ import asyncio
 import aiohttp
 
 from lib.configuration import config
+from lib.configuration.config import get_int_environment_value
 from lib.sfm.api_call_latency import ApiCallLatency
 
 
@@ -33,7 +34,17 @@ trace_config.on_request_start.append(on_request_start)
 trace_config.on_request_end.append(on_request_end)
 
 
-_default_timeout = aiohttp.ClientTimeout(total=120, connect=120)
+_REQUEST_TIMEOUT_TOTAL = get_int_environment_value("REQUEST_TIMEOUT_TOTAL", 120)
+_REQUEST_TIMEOUT_SOCK_CONNECT = get_int_environment_value("REQUEST_TIMEOUT_SOCK_CONNECT", 30)
+_REQUEST_TIMEOUT_SOCK_READ = get_int_environment_value("REQUEST_TIMEOUT_SOCK_READ", 60)
+_GCP_CONNECTOR_LIMIT = get_int_environment_value("GCP_CONNECTOR_LIMIT", 500)
+
+_default_timeout = aiohttp.ClientTimeout(
+    total=_REQUEST_TIMEOUT_TOTAL,
+    connect=None,
+    sock_connect=_REQUEST_TIMEOUT_SOCK_CONNECT,
+    sock_read=_REQUEST_TIMEOUT_SOCK_READ,
+)
 
 
 def init_dt_client_session() -> aiohttp.ClientSession:
@@ -47,7 +58,7 @@ def init_dt_client_session() -> aiohttp.ClientSession:
 
 
 def init_gcp_client_session() -> aiohttp.ClientSession:
-    connector = aiohttp.TCPConnector(enable_cleanup_closed=True)
+    connector = aiohttp.TCPConnector(enable_cleanup_closed=True, limit=_GCP_CONNECTOR_LIMIT)
     return aiohttp.ClientSession(
         connector=connector,
         timeout=_default_timeout,
